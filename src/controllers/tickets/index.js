7544f1661e93355fe49d7b406e6413694f79faf7
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-20 03:57:52 +0000

    update do controller

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,13 +1,18 @@
 export function index({ request, response, database }) {
-  const table = database.select("tickets");
+  // passo o nome do objeto para receber o valor
+  const { status } = request.query;
+
+  const filters = status ? { status } : null;
+
+  const table = database.select("tickets", filters);
 
   response.end(JSON.stringify(table));
 }
 
 /*
-* CREATE - criar
-* INDEX - listar 
-* UPDATE - atualizar
-* DELETE - remover
-* SHOW - para exibir um único registro
-*/
+ * CREATE - criar
+ * INDEX - listar
+ * UPDATE - atualizar
+ * DELETE - remover
+ * SHOW - para exibir um único registro
+ */
