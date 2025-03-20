245108f16749989598d91bde0fac7f6ba12a940a
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-20 03:58:36 +0000

    update do middleware de rotas

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,15 +1,22 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    console.log(route);
     return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path);
+
+    const { query } = routeParams.groups;
+
+    // Se tiver query, se não devolve um objeto vazio
+    request.query = query ? extractQueryParams(query) : {}
+
     return route.controller({ request, response, database });
   }
 
