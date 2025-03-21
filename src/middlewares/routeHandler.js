53a31a136c730593d70f138aed835d3d731e58a5
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-21 15:25:39 +0000

    Update da pasta de middlewares, arquivo

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -12,10 +12,13 @@ export function routeHandler(request, response) {
   if (route) {
     const routeParams = request.url.match(route.path);
 
-    const { query } = routeParams.groups;
+    const { query, ...params } = routeParams.groups;
+
+    // Despejando todo os resto que tem dentro de groups em `params`
+    request.params = params;
 
     // Se tiver query, se não devolve um objeto vazio
-    request.query = query ? extractQueryParams(query) : {}
+    request.query = query ? extractQueryParams(query) : {};
 
     return route.controller({ request, response, database });
   }
